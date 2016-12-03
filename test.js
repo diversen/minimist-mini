23a13580fe5ba603b73fac146d32862c28beeda6
Dennis Iversen <dennis.iversen@gmail.com>
2016-12-03 16:51:23 +0100

    fix help

--- test.js
+++ test.js
@@ -2,11 +2,12 @@ var mini = require('./index');
 
 // Opts are the same as in minimist
 var opts = [];
-opts.boolean = ['boolean'];
-opts.string = ['help', 'test'];
+opts.boolean = ['help'];
+opts.string = ['test'];
 
 // Create object
 var m = new mini(opts);
+console.log(m);
 
 // Check if test is set
 var test = m.get('test');
@@ -21,6 +22,7 @@ if (test){
 // You can also set a file from current path, e.g.
 // 'docs/cli.md
 //
+
 if (m.get('help')) {
     m.helpMessage();
 }
